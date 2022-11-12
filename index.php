deb6d878bc3e0049fd7953f761ad33d20dbced14
mohdarshad86 <mohdarshad8668@gmail.com>
2022-11-12 21:34:39 +0530

    hosted on webhost

--- index.php
+++ index.php
@@ -158,7 +158,7 @@ $data = mysqli_fetch_assoc($result);
                 <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span><?= $data['age'] ?></span></li>
                 <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span><?= $data['degree'] ?></span></li>
                 <li><i class="bi bi-chevron-right"></i> <strong>Certification:</strong> <span><?= $data['certification'] ?></span></li>
-                <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span><?= $data['email'] ?></span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span><?= $data['email'] ?></span></li>
                 <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>
 
                     <?php
@@ -320,40 +320,40 @@ $data = mysqli_fetch_assoc($result);
             <h4>Bachelor of Technology &amp; Web development</h4>
             <h5>2017 - 2021</h5>
             <p><em>Kamla Nehru Institute of Technology, Sultanpur, UP</em></p>
-            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+            <p>I have completed my graduation from KNIT Sultanpur</p>
           </div>
           <div class="resume-item">
             <h4>SSC &amp; Mathematics</h4>
             <h5>2012 - 2014</h5>
             <p><em>HDB MEMO IC, Shishgarh, Bly</em></p>
-            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+            <p>Senior Secondary was a bit challenging year for me, I have to preapre for entrance Exams and School simultaneously</p>
           </div>
         </div>
         <div class="col-lg-6">
           <h3 class="resume-title">Professional Experience</h3>
           <div class="resume-item">
-            <h4>Senior graphic design specialist</h4>
-            <h5>2019 - Present</h5>
-            <p><em>Experion, New York, NY </em></p>
+            <h4>Java Developer</h4>
+            <h5>Sept 2022 - Oct 2022</h5>
+            <p><em>Nagpur, Maharashtra, India </em></p>
             <p>
             <ul>
-              <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+              <li>Lead in the design, Web development, and implementation of the php, layout, and production communication materials</li>
               <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-              <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+              <li>Supervise the assessment of all Web Development materials in order to ensure quality and accuracy of the design</li>
               <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
             </ul>
             </p>
           </div>
           <div class="resume-item">
-            <h4>Graphic design specialist</h4>
-            <h5>2017 - 2018</h5>
-            <p><em>Stepping Stone Advertising, New York, NY</em></p>
+            <h4>Cyber Cell Incharge</h4>
+            <h5>2018 - 2020</h5>
+            <p><em>KNIT Sultanpur, India</em></p>
             <p>
             <ul>
-              <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-              <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-              <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-              <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              <li>I developed an mobile responsive online game website named Tale of Crypton.</li>
+              <li>Managed up to 5 projects or tasks at a given time while under pressure of my studies</li>
+              <li>Also responsible for hacking and threat prevention</li>
+              <li>Created 4+ design presentations and proposals a month for ISTE.</li>
             </ul>
             </p>
           </div>
